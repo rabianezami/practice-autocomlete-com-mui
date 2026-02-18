2e573cb4e899a0c0a40212d565fbefc472514564
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-02-18 20:36:34 +0430

    feat: Add fake top100Films component for test

---
+++ src/top100Films.js
@@ -0,0 +1,8 @@
+const top100Films = [
+  { label: "The Shawshank Redemption", year: 1994 },
+  { label: "The Godfather", year: 1972 },
+  { label: "The Dark Knight", year: 2008 },
+  { label: "Pulp Fiction", year: 1994 },
+];
+
+export default top100Films;
